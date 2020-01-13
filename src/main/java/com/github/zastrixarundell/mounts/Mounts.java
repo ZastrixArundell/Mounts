9b232fac2fd0a826a344a8e1f28f6260975e2223
ZastrixArundell <zastrixarundell@gmail.com>
2020-01-13 19:30:57 +0100

    Init commit

---
+++ src/main/java/com/github/zastrixarundell/mounts/Mounts.java
@@ -0,0 +1,18 @@
+package com.github.zastrixarundell.mounts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Mounts extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable()
+    {
+        super.onDisable();
+    }
+}
