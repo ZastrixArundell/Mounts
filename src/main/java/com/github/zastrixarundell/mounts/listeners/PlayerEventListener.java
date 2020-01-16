6c5c413d46a716575f930eab4e33087ab0f74d1e
ZastrixArundell <zastrixarundell@gmail.com>
2020-01-16 22:54:10 +0100

    [#170684112] Added GUI for horses

--- src/main/java/com/github/zastrixarundell/mounts/listeners/PlayerEventListener.java
+++ src/main/java/com/github/zastrixarundell/mounts/listeners/PlayerEventListener.java
@@ -3,6 +3,9 @@ package com.github.zastrixarundell.mounts.listeners;
 import com.github.zastrixarundell.mounts.Mounts;
 import com.github.zastrixarundell.mounts.entities.Mount;
 import com.github.zastrixarundell.mounts.entities.Rider;
+import com.github.zastrixarundell.mounts.values.MountType;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,6 +13,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerEventListener implements Listener
 {
@@ -43,7 +47,7 @@ public class PlayerEventListener implements Listener
     private void removePlayerBufferOnQuit(PlayerQuitEvent event) { Rider.deleteRiderBuffer(event.getPlayer()); }
 
     @EventHandler
-    private void disableGuiClick(InventoryClickEvent event)
+    private void disableGuiHorseClick(InventoryClickEvent event)
     {
         InventoryHolder holder = event.getInventory().getHolder();
 
@@ -59,5 +63,36 @@ public class PlayerEventListener implements Listener
             if (event.getWhoClicked() instanceof Player)
                 ((Player) event.getWhoClicked()).updateInventory();
         }
+    }
+
+    @EventHandler
+    private void disableGuiMenuClick(InventoryClickEvent event)
+    {
+        if(!event.getView().getTitle().startsWith("Your mounts - page"))
+            return;
+
+        event.setCancelled(true);
+
+        Player player = (Player) event.getWhoClicked();
+        player.updateInventory();
+
+        if (event.getCurrentItem() == null)
+            return;
+
+        if (event.getCurrentItem().getItemMeta() == null)
+            return;
+
+        ItemStack item = event.getCurrentItem();
+
+        if(item.getType() != Material.SADDLE)
+            return;
+
+        String rawName = ChatColor.stripColor(item.getItemMeta().getDisplayName().replace(" ", "_").toUpperCase());
+
+        player.closeInventory();
+
+        Rider rider = Rider.asRider(((Player) event.getWhoClicked()));
+
+        new Mount(player, rider.getSpeed(), MountType.valueOf(rawName)).spawn();
     }
 }
